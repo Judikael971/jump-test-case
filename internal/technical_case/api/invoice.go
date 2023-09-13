29934764c24c5c70d7792f09c8a3cc52343cc30a
jaucagos <jaucagos@free-infra.fr>
2023-09-13 22:07:54 +0200

    Add Invoices API endpoint

---
+++ internal/technical_case/api/invoice.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"jump/technical-case/internal/technical_case/models"
+	"jump/technical-case/internal/util/connectors"
+	"jump/technical-case/internal/util/validators"
+	"net/http"
+)
+
+func PostInvoice(c *gin.Context) {
+	var queryData = validators.InvoiceRequest{}
+	if err := c.ShouldBind(&queryData); err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	err, user := findUser(queryData.UserId)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	invoice := models.Invoices{UserId: user.Id, Status: "pending", Label: queryData.Label, Amount: roundFloat(queryData.Amount)}
+	inserted := connectors.Connector.Create(&invoice)
+	if inserted.Error != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.AbortWithStatus(http.StatusNoContent)
+}
+
+func findInvoice(id int) (error, *models.Invoices) {
+	var invoice models.Invoices
+	connectors.Connector.First(&invoice, id)
+	if invoice.Id == 0 {
+		return gorm.ErrRecordNotFound, &invoice
+	}
+	return nil, &invoice
+}
