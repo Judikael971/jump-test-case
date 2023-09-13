8e05cf9c2d528f7e9109abeae3b5ccb98d508ee0
jaucagos <jaucagos@free-infra.fr>
2023-09-13 22:08:20 +0200

    Add Users API endpoint

---
+++ internal/technical_case/api/users.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"jump/technical-case/internal/technical_case/models"
+	"jump/technical-case/internal/util/connectors"
+	"net/http"
+)
+
+func GetUsers(c *gin.Context) {
+	var user []models.Users
+	result := connectors.Connector.Find(&user)
+	if result.Error != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.AbortWithStatusJSON(http.StatusOK, user)
+}
+
+func findUser(id int) (error, *models.Users) {
+	var user models.Users
+	connectors.Connector.First(&user, id)
+	if user.Id == 0 {
+		return gorm.ErrRecordNotFound, &user
+	}
+	return nil, &user
+}
